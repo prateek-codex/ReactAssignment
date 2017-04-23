93682db34e0e701771b223c64e3a5d073e1e0727
Prateek Varshney <prateek.varshney@nagarro.com>
2017-04-23 18:46:28 +0530

    Anonymous

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -11,6 +11,8 @@ LeaderBoard.GetLeaderBoard = function() {
 
 LeaderBoard.UpdateLeaderBoard = function(name) {    
     var position = -1;
+    name = name ? name : 'Anonymous';
+
     for(var i=0; i<LeaderBoard.Data.length; i++) {
         if(LeaderBoard.Data[i].Name.toLowerCase() === name.toLowerCase()) {
             position = i;
