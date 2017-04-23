961c8e01b93afdea98854d4bda11a719f7caba24
test <test@test.com>
2017-04-23 15:35:32 +0530

    changed 2nd method

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const Hapi = require('hapi');
 const https = require('https');
+const serverName = 'https://10.127.128.56:8000';
 
 // Create a server with a host and port
 const server = new Hapi.Server();
@@ -25,7 +26,7 @@ server.route([{
 	method: 'GET',
 	path: '/initialize',
 	handler: (request, reply) => {
-		https.get('https://10.127.128.56:8000/alphabets/getchars/6', (res) => {
+		https.get(serverName + '/alphabets/getchars/6', (res) => {
 			let rawData = '';
 			res.on('data', (chunk) => { rawData += chunk; });
 			res.on('end', () => {
@@ -34,6 +35,27 @@ server.route([{
 					reply(parsedData);
 				} catch (e) {
 					console.error(e.message);
+					reply('');
+				}
+			});
+		});
+	},
+},
+{
+	method: 'GET',
+	path: '/shuffle/{query}',
+	handler: (request, reply) => {
+		let params = request.params.query
+		https.get(serverName + '/alphabets/shuffle/' + params, (res) => {
+			let rawData = '';
+			res.on('data', (chunk) => { rawData += chunk; });
+			res.on('end', () => {
+				try {
+					const parsedData = JSON.parse(rawData);
+					reply(parsedData);
+				} catch (e) {
+					console.error(e.message);
+					reply('');
 				}
 			});
 		});
